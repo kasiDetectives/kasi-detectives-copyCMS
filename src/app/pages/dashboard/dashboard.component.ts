6c533cb12ceb0b1f366b0f0721d357f68416cb00
Willington Mnisi <willington.mnisi@gmail.com>
2019-11-06 14:55:11 +0200

    added reportIncidenceService as a service

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -16,8 +16,30 @@ export class DashboardComponent implements OnInit {
   public clicked1: boolean = false;
   public clicked2: boolean = false;
 
+
+  reason = '';
+  array = []
+  numUserIncidents
+  numPlaceIncidents = []
+  currentYearReports = []
+  monthlyReport = [
+    {month: 'January', numberOfReports: 0},
+    {month: 'February', numberOfReports: 0},
+    {month: 'March', numberOfReports: 0},
+    {month: 'April', numberOfReports: 0},
+    {month: 'May', numberOfReports: 0},
+    {month: 'June', numberOfReports: 0},
+    {month: 'July', numberOfReports: 0},
+    {month: 'August', numberOfReports: 0},
+    {month: 'September', numberOfReports: 0},
+    {month: 'October', numberOfReports: 0},
+    {month: 'November', numberOfReports: 0},
+    {month: 'December', numberOfReports: 0}
+  ]
+
+  
   constructor(public analyticsService : AnalyticsService) {
-    this.analyticsService.getUserReports()
+    this.fetchUserReports()
   }
 
   ngOnInit() {
@@ -470,4 +492,101 @@ export class DashboardComponent implements OnInit {
     this.myChartData.data.datasets[0].data = this.data;
     this.myChartData.update();
   }
+  fetchUserReports(){
+    this.analyticsService.getUserReports().then(data => {
+      console.log(data);
+      let userReports = data
+
+      for(let key in userReports){
+        
+      }
+      
+    })
+  }
+
+  
+
+  // fetchUserReports(){
+  //   this.analyticsService.getUserReports().then(data => {
+  //     console.log(data);
+  //     let userReports = data
+  //     let number = 0
+  //     let addnew = true
+  //     for(let key in userReports){
+  //       console.log(key);
+  //       console.log(Object.values(userReports[key]));
+  //       let info: Array<any> = Object.values(userReports[key])
+  //       console.log(info);
+        
+  //       for(let i=0; i<info.length; i++){
+          
+  //         console.log(info[i]);
+  //         this.array.push({
+  //           place: key,
+  //           details: info[i]
+  //         })
+  //       }
+  //       this.numPlaceIncidents.push({
+  //         place: key,
+  //         numberOfReports: info.length
+  //       })
+  //     }
+  //     // let number = 0
+  //     console.log(this.array);
+  //     for(let x = 0; x < this.array.length; x++){
+  //       console.log('rererererererererre');
+  //       if(this.currentYearReports.length = 0){
+  //         console.log('aading');
+          
+  //         this.currentYearReports.push({
+  //           year: this.array[x].details.year,
+  //           numberOfReports: number
+  //         })
+  //       }
+        
+  //       for(let y = 0; y < this.currentYearReports.length; y++){
+  //         if(this.array[x].details.year !== this.currentYearReports[y].year){
+            
+  //           addnew = true
+  //           console.log(addnew);
+            
+  //         }
+  //         if(this.array[x].details.year === this.currentYearReports[y].year){
+            
+  //           addnew = false
+  //           console.log(addnew);
+  //         }
+  //       }
+  //       for(let y = 0; y < this.currentYearReports.length; y++){
+  //         if(addnew === true){
+  //           this.currentYearReports.push({
+  //             year: this.array[x].details.year,
+  //             numberOfReports: number
+  //           })
+  //         } else {
+  //           this.currentYearReports[y].numberOfReports = (number + 1)
+  //         }
+  //       }
+
+        
+        
+        
+  //     }
+
+      
+  //     console.log(this.array);
+  //     console.log(this.numPlaceIncidents);
+      
+  //     this.numUserIncidents = this.array.length
+  //       console.log(this.numUserIncidents + ' reported user incidents')
+  //       console.log();
+  //       console.log(this.currentYearReports);
+        
+        
+        
+      
+  //   })
+
+
+  // }
 }
--- src/services/analytics.service.ts
+++ src/services/analytics.service.ts
@@ -13,6 +13,6 @@ export class AnalyticsService {
       let values = snapshot.val()
       return values
     })
-    \
+    
   }
 }
